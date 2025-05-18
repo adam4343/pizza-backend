dfdc5ccc81f192ed2c150dfede08b37171821488
Adam Ingor <adamingor@192.168.100.27>
2025-05-18 20:52:43 +0300

    Migrated to  turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,37 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+// import 'dotenv/config';
+// import { defineConfig } from 'drizzle-kit';
+
+// export default defineConfig({
+//   out: './drizzle',
+//   schema: [
+//     "./src/db/schemas/cart.schema.ts",
+//     "./src/db/schemas/order.schema.ts",
+//     "./src/db/schemas/pizza.schema.ts",
+//     "./src/db/schemas/user.schema.ts"
+//   ],
+//   driver: '',
+//   dbCredentials: {
+//     url: process.env.TURSO_DATABASE_URL!,
+//     authToken: process.env.TURSO_AUTH_TOKEN!,
+//   },
+// });
+
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+const authToken = process.env.TURSO_DB_AUTH_TOKEN;
 
 export default defineConfig({
-  out: './drizzle',
+  out: "./drizzle",
   schema: [
-    "./src/db/schemas/cart.schema.ts",
-    "./src/db/schemas/order.schema.ts",
-    "./src/db/schemas/pizza.schema.ts",
-    "./src/db/schemas/user.schema.ts"
-  ],
-  dialect: 'sqlite',
+         "./src/db/schemas/cart.schema.ts",
+         "./src/db/schemas/order.schema.ts",
+         "./src/db/schemas/pizza.schema.ts",
+         "./src/db/schemas/user.schema.ts"
+       ],
+  dialect: authToken ? "turso" : "sqlite",
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: process.env.TURSO_DB_URL || "file:dev.db",
+    authToken: process.env.TURSO_DB_AUTH_TOKEN,
   },
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -4,12 +4,16 @@ import * as cartSchema from "./schemas/cart.schema";
 import * as userSchema from "./schemas/user.schema";
 import * as pizzaSchema from "./schemas/pizza.schema";
 import * as orderSchema from "./schemas/order.schema";
+import { createClient } from '@libsql/client';
 
-export const db = drizzle(process.env.DB_FILE_NAME!, {
-    schema: {
-        ...cartSchema,
-        ...userSchema,
-        ...pizzaSchema,
-        ...orderSchema
-    }
+export const tursoClient = createClient({
+  url: process.env.TURSO_DB_URL!, 
+  authToken: process.env.TURSO_DB_AUTH_TOKEN, 
 });
+
+export const db = drizzle(tursoClient, { schema: {
+    ...cartSchema,
+    ...userSchema,
+    ...pizzaSchema,
+    ...orderSchema
+} });
